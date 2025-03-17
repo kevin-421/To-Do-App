8b9242233e90a1e19bb1241c4984e62f88c1b22e
unknown <kevinpena421@gmail.com>
2025-03-17 17:59:04 -0500

    went ahead and got the js to print out input to the DOM

--- todo.js
+++ todo.js
@@ -6,14 +6,14 @@ function NewTask() {
     // value from input
     const newText = document.getElementById('inputText').value
     // div element for new task
-    const NewTodo = document.getElementsByClassName('newTodo')
+    const NewTodo = document.querySelector('.newTodo')
     // new h1 element for new task
     const NewElement = document.createElement('h1')
         NewElement.setAttribute('id','1')
-        NewElement.textContent = 'hi'
-        NewElement.appendChild(NewElement)
-        document.getElementsByClassName('newTodo').appendChild(NewElement)
-        
+        // text content from input to h1
+        NewElement.textContent = newText
+        // appending to container
+        NewTodo.append(NewElement)        
     
 }
 
