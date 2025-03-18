132a793da311855171f20ce8e76376665c362944
unknown <kevinpena421@gmail.com>
2025-03-17 22:11:15 -0500

    added edit & delete buttons to the code

--- todo.js
+++ todo.js
@@ -1,19 +1,46 @@
-const clickEvent = document.getElementById('btn').addEventListener('click',NewTask)
+const newTaskEvent = document.getElementById('btn').addEventListener('click',NewTask)
+const editEvent = document.getElementById('btn').addEventListener('click',editTask)
+const deleteEvent = document.getElementById('btn').addEventListener('click',deleteTask)
 
 
+const NewTodo = document.querySelector('.newTodo')
 
 function NewTask() {
     // value from input
     const newText = document.getElementById('inputText').value
-    // div element for new task
-    const NewTodo = document.querySelector('.newTodo')
     // new h1 element for new task
     const NewElement = document.createElement('h1')
-        NewElement.setAttribute('id','1')
+        NewElement.setAttribute('id',`${newText}`)
         // text content from input to h1
         NewElement.textContent = newText
+        // editing
+
         // appending to container
-        NewTodo.append(NewElement)        
-    
+        NewTodo.append(NewElement)
+        console.log(NewElement.HTMLCollection);
+        
+}
+
+function editTask() {
+    // new button for editing task
+    const newButton = document.createElement('button')
+        newButton.innerHTML = 'edit'
+        // styling edit btn
+        newButton.style.backgroundColor = 'green'
+        // append to container
+        NewTodo.append(newButton)
+
+        
+
+}
+
+function deleteTask() {
+    // new button for deleting task 
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'delete'
+    // styling delete btn
+        deleteButton.style.backgroundColor = 'red'
+        // append to container
+    NewTodo.append(deleteButton)
 }
 
